ace21dd9706df69edc9b454b763b76c1cb63994d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-09-26 08:28:48 -0500

    [FEATURE] coinstack with screen

--- src/components/coins/CoinsStack.js
+++ src/components/coins/CoinsStack.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
+import CoinsScreen from "./CoinsScreen";
 
 const stack = createStackNavigator();
 
 const CoinStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Coins" />
+      <Stack.Screen name="Coins" component={CoinsScreen} />
     </Stack.Navigator>
   );
 };
